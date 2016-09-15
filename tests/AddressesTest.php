6111b3b0d9342292a1b8c5e9c9bbf8faef531807
Nicolaj Egelund <nicomanden@gmail.com>
2016-09-15 09:53:55 +0200

    Added tests for pagination

--- tests/AddressesTest.php
+++ tests/AddressesTest.php
@@ -12,6 +12,20 @@ class AddressesTest extends TestCase {
         $this->assertGreaterThanOrEqual(1, count($addresses));
     }
 
+    public function testPagination() {
+        $dawa = new Dawa;
+
+        $addresses = (new Dawa(2, 1))->addressesInCircle("55.245589", "10.469713", "100");
+
+        $this->assertEquals('Hallingager 2, 5750 Ringe', $addresses[0]->adressebetegnelse);
+        $this->assertEquals(2, count($addresses));
+
+        $addresses = $dawa->paginate(2, 2)->addressesInCircle("55.245589", "10.469713", "100");
+        
+        $this->assertEquals('Hallingager 12, 5750 Ringe', $addresses[0]->adressebetegnelse);
+        $this->assertEquals(2, count($addresses));
+    }
+
     public function testAddressesInCircle() {
         $dawa = new Dawa;
 
